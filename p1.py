68e7f13b5da1f281de1a7e4332b375cab4fe81ab
pashaarshad <arshadpashaintern@gmail.com>
2026-05-13 19:39:08 +0530

    first commit

---
+++ p1.py
@@ -0,0 +1,29 @@
+g={
+        'A':['B','C'],
+        'B':['D','E'],
+        'C':['F'],
+        'D':[],
+        'E':['F'],
+        'F':[]
+
+
+    }
+
+v = []
+q = []
+
+def bfs(v,g,n):
+    v.append(n)
+    q.append(n)
+
+    while q:
+        s=q.pop(0)
+        print(s,end="")
+
+        for nib in g[s]:
+            if nib not in v:
+                v.append(nib)
+                q.append(nib)
+
+
+bfs(v,g,'A')
